7c50c972a1b557a97bd905353c1d48929914ed72
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-28 11:50:11 +1300

    Solved

---
+++ Number 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//6) Write a Program to Swap Two Numbers
+
+namespace Number_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1 = 32, n2=23;
+            int n3; 
+
+            n3 = n1;
+            n1 = n2;
+            n2 = n3;
+
+            Console.WriteLine("swapped numbers {0} {1}", n1,n2 );
+
+            Console.ReadLine();
+
+        }
+    }
+}
