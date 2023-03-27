a63fe688199fe795afe5155a22dd3f0c456a3ba5
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-28 11:18:55 +1300

    Number 3

---
+++ Number 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//3) Write a Program to Add Two Integer Numbers Entered by User
+
+namespace Number_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Following is the program to add two number entered by the user Please enter your number");
+
+
+            Console.WriteLine("\n");
+
+            Console.Write("Please enter your number: ");
+            int num1= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            Console.Write("Please enter your Second number: ");
+            int num2= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            int num3 = num1 + num2;
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Following are your two numbers: {0} + {1}",num1,num2);
+
+            Console.WriteLine("\n");
+
+            Console.ReadLine();
+
+            Console.WriteLine("This is the sum of your numbers {0}", num3);
+
+            Console.ReadLine();
+
+        }
+    }
+}
