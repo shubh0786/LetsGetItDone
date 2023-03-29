b10531b89fd20fe59b6f1b7a1606afa2ef082b6d
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-29 14:28:06 +1300

    ConorVovel

---
+++ ChrOrVowel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a Program to Check Whether a Character isVowel or Consonant
+
+namespace ChrOrVowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.WriteLine("Please enter your chracter");
+
+            ch = Char.ToLower(Console.ReadKey().KeyChar);
+
+            Console.WriteLine("\n");
+
+            if (ch == 'a'|| ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                Console.WriteLine("{0} is a vowel",ch);
+            }
+            else
+            {
+                Console.WriteLine("{0} is consonant",ch);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
