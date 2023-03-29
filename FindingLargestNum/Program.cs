9ab9fe047d15f5ea846a9f99d269560b5bb6c8ec
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-29 14:44:38 +1300

    Solved Largestnumber

---
+++ FindingLargestNum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingLargestNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1,n2,n3;
+
+       
+
+
+            Console.WriteLine("Please enter your number");
+            n1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Please enter your number");
+            n2 = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Please enter your number");
+            n3 = int.Parse(Console.ReadLine());
+
+
+            if (n1>n2 && n1>n3)
+            {
+
+                Console.WriteLine("{0} is the largest number",n1);
+
+            } else if (n2>n1 && n2 > n3)
+            {
+                Console.WriteLine("{0} is the largest number",n2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is the largest number",n3);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
