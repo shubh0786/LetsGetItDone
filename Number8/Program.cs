98e77806028ecbf32e8552ad7105298d951ccfc8
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-29 14:16:06 +1300

    IfProblem1

---
+++ Number8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Write a Program to Multiply two decimal Numbersentered by User
+namespace Number8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal num1;
+            decimal num2;
+
+            decimal num3;
+           
+           
+
+            Console.WriteLine("Please enter your number");
+            num1= decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number");
+            num2= decimal.Parse(Console.ReadLine());
+
+
+            num3 = num1 * num2;
+
+            Console.WriteLine("Following are the results: {0} x {1} = {2} ", num1, num2, num3);
+            Console.ReadLine(); 
+
+        }
+    }
+}
---
+++ OddorEven/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a Program to Check Whether Number is Even orOdd
+
+namespace OddorEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+         
+
+
+            Console.WriteLine("Please enter your number");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+
+                Console.WriteLine("Your number is even : {0}", num1);
+
+            }
+            else
+            {
+
+                Console.WriteLine("Your number is odd : {0}", num1);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
